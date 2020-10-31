0a700d9d852a11f94f0cdb72728afb954789901d
Akileshkumar KR <akilesh2003@gmail.com>
2020-10-31 11:28:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello! Welcome to my free Circle Area and Circumference Calculator!")
+pi = 3.14
+
+radius = float(input("Please enter the radius of the circle in any unit: "))
+area = pi*radius
+circ = 2*pi*radius
+print("The Area is {} unit square and the Circumference is {} units".format(area, circ))
